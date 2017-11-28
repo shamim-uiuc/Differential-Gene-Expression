cdb5f2388a6d06855adfd6816f3df22c99cae687
Md Shamimuzzaman <33701852+shamim-uiuc@users.noreply.github.com>
2017-11-28 18:42:37 -0500

    Update Analysis_DEG_edgeR.R

--- Analysis_DEG_edgeR.R
+++ Analysis_DEG_edgeR.R
@@ -9,36 +9,35 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite("edgeR")
 library("edgeR")
-y = read.table("dexample.txt", header=TRUE, row.names=1)  
+#Reading read counts data from a txt file
+y = read.table("Input.txt", header=TRUE, row.names=1)
+#Grouping read count columns into control(ctrl) and treatment as there are 3 replicates  
 group=c("ctrl", "ctrl", "ctrl", "treatment","treatment","treatment")
 dge = DGEList(counts = y, group = group)
+#Estimate common dispersion
 dge = estimateCommonDisp(dge)
+# Estimate Tagwise dispersion
 dge = estimateTagwiseDisp(dge)
+#Perform pair-wise tests for differential expression between two groups.
 et = exactTest(dge)
-
-## Extract results from edgeR analysis
+#topTags returns the top differentially expressed genes
 resEdgeR = topTags(et)
-resEdgeR
-resEdgeR = topTags(et, n=201)
-write.csv(resEdgeR,"Avg_24H_edgeR_output.csv")
+#Wanted to see the results for all of my genes (1100)
+resEdgeR = topTags(et, n=1100)
+resEdgeR=data.frame(resEdgeR)
+# Subsetting to get DE genes with corrected/adjusted p-value <= 0.05, here it is referred as FDR.
+resEdgeR=subset(resEdgeR, resEdgeR$FDR<=0.05)
+head(resEdgeR)
+write.csv(resEdgeR,"DE_genes_edgeR.csv")
 
-With Replicate
-y = read.csv("72H_Reps.csv", header=TRUE, row.names=1)  
-group=c("ctrl", "ctrl", "ctrl", "treatment","treatment","treatment")
-dge = DGEList(counts = y, group = group)
-dge = estimateCommonDisp(dge)
-dge = estimateTagwiseDisp(dge)
-et = exactTest(dge)
-resEdgeR = topTags(et, n=201)
-write.csv(resEdgeR,"Reps_72H_edgeR_output.csv")
 
 
 Without replicate
-y = read.csv("24H.csv", header=TRUE, row.names=1) 
+y = read.table("Input.txt", header=TRUE, row.names=1)
 group=c("ctrl", "treatment")
 dge = DGEList(counts = y, group = group)
 dge = estimateGLMCommonDisp(dge,method="deviance",robust=TRUE,subset = NULL)
 dge = estimateGLMTagwiseDisp(dge)
 et = exactTest(dge)
-resEdgeR = topTags(et, n=201)
-write.csv(resEdgeR,"24H_edgeR_output.csv")
+resEdgeR = topTags(et, n=1100)
+write.csv(resEdgeR,"DE_genes_edgeR.csv")
